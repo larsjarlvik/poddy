95e318157a702bff18fb5af687fc44de0e648d02
larsjarlvik <lars.jarlvik@gmail.com>
2018-09-23 23:09:04 +0700

    Fixed paths

--- android/app/src/main/java/com/crappypay/poddy/MainActivity.java
+++ android/app/src/main/java/com/crappypay/poddy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.crappypay.poddy;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
