933616ada7c603d40b79d77ba0f1bfaf273a5414
larsjarlvik <lars.jarlvik@gmail.com>
2018-09-23 23:56:51 +0700

    Don't flash splash too quick

--- android/app/src/main/java/com/crappypay/poddy/MainActivity.java
+++ android/app/src/main/java/com/crappypay/poddy/MainActivity.java
@@ -1,8 +1,12 @@
 package com.crappypay.poddy;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import java.util.concurrent.TimeUnit;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -11,6 +15,17 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().setStatusBarColor(0x00000000);
+      getWindow().setNavigationBarColor(0x00000000);
+      getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
+    try {
+      TimeUnit.MILLISECONDS.sleep(500);
+    } catch (InterruptedException e) {
+    }
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
