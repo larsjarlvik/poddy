da7c18f74c10d104eaf9df484b676b673a7f1561
larsjarlvik <lars.jarlvik@gmail.com>
2018-09-25 08:26:39 +0700

    Add tap effect to podcast tile

--- lib/components/podcast_tile.dart
+++ lib/components/podcast_tile.dart
@@ -3,14 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:poddy/models/podcast.dart';
 
 Widget buildPodcastTile (Podcast podcast, VoidCallback onTap) {
-  return new GestureDetector(
-    onTap: onTap,
-    child: new Card(
-      elevation: 1.5,
-      child: new CachedNetworkImage(
-        placeholder: new Icon(Icons.photo, color: new Color.fromARGB(255, 56, 56, 56)), 
+  return new Card(
+    elevation: 1.5,
+    child: Stack(children: [
+      new CachedNetworkImage(
+        placeholder: new Icon(Icons.photo, color: new Color.fromARGB(255, 56, 56, 56)),
         imageUrl: podcast.artworkLarge
+      ),
+      new Positioned.fill(
+        child: new Material(
+          color: Colors.transparent,
+          child: new InkWell(
+            highlightColor: Color.fromARGB(100, 255, 255, 255),
+            onTap: onTap,
+          )
+        )
       )
-    )
+    ]),
   );
 }
