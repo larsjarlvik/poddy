b7a30152a8934238807ecea3239074266a3c0a86
larsjarlvik <lars.jarlvik@gmail.com>
2018-10-05 14:15:27 +0700

    Improve read write subscription performance

--- lib/storage/subscriptions.dart
+++ lib/storage/subscriptions.dart
@@ -2,67 +2,93 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:poddy/api/feed.dart';
 import 'package:poddy/models/podcast.dart';
 
-const String _fileName = 'subscriptions.json';
+_encode(Podcast podcast) {
+  return json.encode(podcast);
+}
 
-Future<File> get _localFile async {
+_decode(String podcast) {
+  return json.decode(podcast);
+}
+
+Future<File> getFile(Podcast podcast) async {
   final directory = await getApplicationDocumentsDirectory();
   final path = directory.path;
-  return new File('$path/$_fileName');
+  return new File('$path/sub_${podcast.id}.json');
+}
+
+Future<List<File>> listSubscriptions() async {
+  final directory = await getApplicationDocumentsDirectory();
+  final files = directory.list();
+
+  return files
+    .where((s) => s is File && s.path.contains('sub_'))
+    .map((s) => s as File)
+    .toList();
 }
 
 addSubscription(Podcast podcast) async {
-  final existing = await readSubscriptions();
-  existing.add(podcast);
+  final exists = await containsPodcast(podcast);
+  if (exists) {
+    return;
+  }
 
-  final encoded = json.encode(existing);
-  final file = await _localFile;
+  final encoded = await compute(_encode, podcast);
+  final file = await getFile(podcast);
   await file.writeAsString(encoded);
 }
 
 removeSubscription(Podcast podcast) async {
-  final existing = await readSubscriptions();
-  existing.remove(existing.firstWhere((s) => s.id == podcast.id));
+  final exists = await containsPodcast(podcast);
+  if (!exists) {
+    return;
+  }
 
-  final encoded = json.encode(existing);
-  final file = await _localFile;
-  await file.writeAsString(encoded);
+  final file = await getFile(podcast);
+  file.delete();
 }
 
 updateSubscription(Podcast podcast) async {
   try {
-    final existing = await readSubscriptions();
-    final index = existing.indexWhere((s) => s.id == podcast.id);
-
-    existing[index] = podcast;
-
-    final encoded = json.encode(existing);
-    final file = await _localFile;
+    final file = await getFile(podcast);
+    final encoded = await compute(_encode, podcast);
     await file.writeAsString(encoded);
   } catch (e) {
     print('Failed to update ${podcast.feedUrl}');
   }
 }
 
-Future<List<Podcast>> readSubscriptions() async {
-  final file = await _localFile;
-  final exists = await file.exists();
+Future<Podcast> readSubscription(Podcast podcast) async {
+  final file = await getFile(podcast);
+  return await _readSubscription(file);
+}
 
-  if (!exists) return new List<Podcast>();
-
+Future<Podcast> _readSubscription(File file) async {
   final encoded = await file.readAsString();
-  Iterable podcasts = json.decode(encoded);
-  final mapped = podcasts.map((final p) => Podcast.fromJson(p)).toList();
+  final decoded = await compute(_decode, encoded);
+  return Podcast.fromJson(decoded);
+}
+
+Future<List<Podcast>> readSubscriptions() async {
+  final files = await listSubscriptions();
+  final mapped = new List<Podcast>();
+
+  for (final f in files) {
+    mapped.add(await _readSubscription(f));
+  }
 
   return mapped;
 }
 
 Future<bool> containsPodcast(Podcast podcast) async {
-  final subscriptions = await readSubscriptions();
-  return subscriptions.any((s) => s.id == podcast.id);
+  final subscriptions = await listSubscriptions();
+  final file = await getFile(podcast);
+
+  return subscriptions.where((s) => s.path == file.path).length > 0;
 }
 
 Future refreshPodcast(Podcast podcast) async {
